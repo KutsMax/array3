2cac89839e9ef78f59bfab2f2918e9e27085d438
Maxim Kuts <makskutss@gmail.com>
2021-11-09 19:59:35 +0200

    Change TK_2.py

--- TK_2.py
+++ TK_2.py
@@ -1,10 +1,3 @@
 def tuple_minmax(list_data):
-    _min = list_data[0]
-    _max = list_data[0]
-    for i in range(len(list_data)):
-        if _min > list_data[i]:
-            _min = list_data[i]
-        if _max < list_data[i]:
-            _max = list_data[i]
-    min_max = (_min, _max)
+    min_max = (min(list_data), max(list_data))
     return min_max
